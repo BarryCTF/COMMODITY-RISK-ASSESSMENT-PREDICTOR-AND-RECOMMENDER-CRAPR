506f8391463b8de11b09654e7c9f4e1b0d933f38
BarryCTF <barrychuatf@gmail.com>
2024-08-22 14:10:40 +0800

    Create metals.py

---
+++ metals.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+
+st.title("Capstone Project: Metal Recommender")
+
+
+risk_data = pd.read_csv("data-risk-portrait.csv")
+st.write(risk_data.head(50))
