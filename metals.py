e27781a5ebc3ad2097d0e35de53b688164ee94dd
BarryCTF <barrychuatf@gmail.com>
2024-08-22 15:19:13 +0800

    Update

--- metals.py
+++ metals.py
@@ -9,3 +9,5 @@ st.title("Capstone Project: Metal Recommender")
 
 risk_data = pd.read_csv("data-risk-portrait.csv")
 st.write(risk_data.head(100))
+st.text("This Model is to determine Investor's Risk profile and suitable Metal Portfolio")
+
