896eb8f3571aea34316d08443c6f8a77ca32d2b4
BarryCTF <barrychuatf@gmail.com>
2024-08-26 23:01:40 +0800

    Add files via upload

--- metals.py
+++ metals.py
@@ -4,10 +4,35 @@ import numpy as np
 
 
 
-st.title("Capstone Project: Metal Recommender")
+# Title of the project
+st.title("CAPSTONE PROJECT PHASE I")
+
+# Welcome message
+st.header("WELCOME TO COMMODITIES METALS PORTFOLIO RECOMMENDER")
+
+
+# Action button
+if st.button("CLICK HERE TO PROCEED"):
+    st.write("Bringing you to next page.")
+
+
+# Prototype disclaimer on bottom right of page
+st.markdown(
+    """
+    <style>
+    .bottom-right {
+        position: fixed;
+        bottom: 10px;
+        right: 10px;
+        font-weight: bold;
+    }
+    </style>
+    <div class="bottom-right">**This is still a prototype. Subject to changes**</div>
+    """,
+    unsafe_allow_html=True
+)
+
 
 
 risk_data = pd.read_csv("data-risk-portrait.csv")
 st.write(risk_data.head(100))
-st.text("This Model is to determine Investor's Risk profile and suitable Metal Portfolio")
-
